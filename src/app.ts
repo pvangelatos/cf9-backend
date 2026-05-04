e43f9baac4f91ade82b8a982005bfe1fe026e508
mkarampatsis <marka@central.ntua.gr>
2026-05-04 19:22:26 +0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,15 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
 import { setupSwagger } from './swagger';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+  origin:['http://localhost:4200', 'http://render.cccccc.com']  
+}))
 
 setupSwagger(app);
 app.use(express.json())
