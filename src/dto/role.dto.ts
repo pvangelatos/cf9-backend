8dcc5c423910b0b97f684ad3f70a9691158e01c2
pvangelatos <p.vangelatos23@hotmail.com>
2026-05-29 09:04:40 +0300

    initialize

--- src/dto/role.dto.ts
+++
@@ -1,6 +0,0 @@
-export interface RoleDTO {
-  id: string;
-  role: string;
-  description:string;
-  active: boolean;
-}
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,6 @@
 import app from './app';
-import { connectDB } from './utils/db';
 
-
-const start = async() => {
-  
-  await connectDB();
-  
-  app.listen(3000, ()=>{
-    console.log('Server is up, 3000');
-  })
-}
-
-start();
+app.listen(3000, () => {
+  console.log('Server is up');
+  console.log('lalakis');
+})
